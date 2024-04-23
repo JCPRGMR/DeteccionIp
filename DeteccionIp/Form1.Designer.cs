d27f168c79e9a80331b0dd04b9f0a014e1f7db63
JCPRGMR <juancardedon@gmail.com>
2024-04-23 12:44:51 -0400

    Agregar archivos de proyecto.

---
+++ DeteccionIp/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace DeteccionIp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnIdetificar = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(174, 64);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(100, 20);
+            this.txtIp.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(87, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Introduzca la IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(86, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tu ip es: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(55, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "La ip es de tipo:";
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(143, 195);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(174, 20);
+            this.txtTipo.TabIndex = 4;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(143, 164);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(174, 20);
+            this.txtCategoria.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(140, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "IDETIFICADOR DE IP\'s";
+            // 
+            // btnIdetificar
+            // 
+            this.btnIdetificar.Location = new System.Drawing.Point(90, 109);
+            this.btnIdetificar.Name = "btnIdetificar";
+            this.btnIdetificar.Size = new System.Drawing.Size(99, 31);
+            this.btnIdetificar.TabIndex = 7;
+            this.btnIdetificar.Text = "IDENTIFICAR";
+            this.btnIdetificar.UseVisualStyleBackColor = true;
+            this.btnIdetificar.Click += new System.EventHandler(this.btnIdetificar_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblError.Location = new System.Drawing.Point(109, 240);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 8;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(195, 109);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(99, 31);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(417, 268);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnIdetificar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnIdetificar;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ DeteccionIp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace DeteccionIp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnIdetificar_Click(object sender, EventArgs e)
+        {
+            string ipString = txtIp.Text;
+
+            if (IPAddress.TryParse(ipString, out IPAddress ip))
+            {
+                // Determinar si es válida y de qué clase es
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    byte[] bytes = ip.GetAddressBytes();
+                    if (bytes[0] >= 1 && bytes[0] <= 126)
+                    {
+                        txtCategoria.Text = "A";
+                        txtTipo.Text = (bytes[0] == 10 ? "privada" : "pública");
+                    }
+                    else if (bytes[0] >= 128 && bytes[0] <= 191)
+                    {
+                        txtCategoria.Text = "B";
+                        txtTipo.Text = (bytes[0] == 172 && bytes[1] >= 16 && bytes[1] <= 31 ? "privada" : "pública");
+                    }
+                    else if (bytes[0] >= 192 && bytes[0] <= 223)
+                    {
+                        txtCategoria.Text = "C";
+                        txtTipo.Text = (bytes[0] == 192 && bytes[1] == 168 ? "privada" : "pública");
+                    }
+                    else
+                    {
+                        lblError.Text = "La IP no pertenece a ninguna clase conocida";
+                    }
+                }
+                else
+                {
+                    lblError.Text = "La IP no es una dirección IPv4 válida";
+                }
+            }
+            else
+            {
+                lblError.Text = "La cadena proporcionada no es una dirección IP válida";
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            txtCategoria.Clear();
+            txtIp.Clear();
+            txtTipo.Clear();
+        }
+    }
+}
